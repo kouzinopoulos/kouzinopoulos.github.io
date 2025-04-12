73f58f645b5be392ac5973fe6d04c4d37ed57399
kouzinopoulos <kouzinopoulos@gmail.com>
2025-04-12 08:47:11 +0000

    Deploying to gh-pages from @ kouzinopoulos/kouzinopoulos.github.io@8231ab1ac98c709a3aa99a4d575c70650c07c40f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-paper-predicting-the-lifespan-of-industrial-printheads-with-survival-analysis-accepted-by-icps-25",
+          title: 'Paper Predicting the Lifespan of Industrial Printheads with Survival Analysis accepted by ICPS’25...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
