42f0ff98b9bc124c953eb86cf2fac79927071f42
kouzinopoulos <kouzinopoulos@gmail.com>
2025-09-02 07:57:19 +0000

    Deploying to gh-pages from @ kouzinopoulos/kouzinopoulos.github.io@1da92e8cb551a35a7930c9569b6903f3c3301548 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "Projects",
+          title: "projects",
           description: "List of current European and national projects",
           section: "Navigation",
           handler: () => {
